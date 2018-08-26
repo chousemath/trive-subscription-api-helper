aaf3f013b559f0c3380c7f075431d70ec3c4f8d3
Joseph Sungpil Choi <chousemath@gmail.com>
2018-08-26 16:05:27 +0900

    Add checks for PaymentUserBillingKey

--- interfaces/payment-user-billing-key.js
+++ interfaces/payment-user-billing-key.js
@@ -1,7 +1,6 @@
 "use strict";
 exports.__esModule = true;
 exports.PaymentUserBillingKeyRequired = [
-    'status',
     'paymentId',
     'userBillingKeyId',
 ];
--- interfaces/payment-user-billing-key.ts
+++ interfaces/payment-user-billing-key.ts
@@ -1,5 +1,4 @@
 export const PaymentUserBillingKeyRequired: Array<string> = [
-  'status',
   'paymentId',
   'userBillingKeyId',
 ];
